50f7a3ecb1579dab4d1177f4148f8ce6b8160872
wwj <22590570@qq.com>
2019-05-24 11:14:39 +0800

    重新根据页码优化抓取流程和注释说明

--- src/com/zr/mm/CatchMM.java
+++ src/com/zr/mm/CatchMM.java
@@ -13,35 +13,32 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class CatchMM {
-//	public static void main(String[] args) {
-//			String first_url = "https://www.duodia.com/daxuexiaohua/";
-//			try {
-//				Document doc = Jsoup.connect(first_url).get();
-//			    Elements  els = doc.select("#main > div.row > article > div > a.thumbnail-container > img");
-//			    for (Element el : els) {
-//					String  mm_Name = el.attr("alt");
-//					String  mm_area = mm_Name.substring(0,2);
-//					String  mm_ImgUrl = el.attr("src");
-//					//saveToFile(mm_ImgUrl, filePath);
-//					String file_path = "D:\\mm\\"+mm_area;
-//					String img_path = "D:\\mm\\"+mm_area+"\\"+mm_Name+".jpg";
-//					saveToFile(mm_ImgUrl, file_path,img_path);
-//				}
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//	}
 	public static void main(String[] args) {
-				getUrl("https://www.duodia.com/daxuexiaohua/");
+		         try {
+					int pageCount = getPageCount("https://www.duodia.com/daxuexiaohua/");
+					int   count = 1;
+					//根据页数总数做递归的终止条件
+					getUrl("https://www.duodia.com/daxuexiaohua/", pageCount, count);
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	}
 	
 	
-	public static void   getUrl(String url) {
+	/**
+	 * 根据路径页面总数，计数 递归获取图片信息
+	 * @param url  路径
+	 * @param pageCount  页面总数
+	 * @param count  起始计数
+	 */
+	public static void   getUrl(String url,int pageCount,int count) {
 		try {
+			if(count<pageCount){
 			Document doc = Jsoup.connect(url).get();
 			Elements  els = doc.select("#main > div.area-pagination.archive-pagination > div > a[title=下一页]");
-			if(els.size()==1) {
+			
 				//拿到当页的数据,获取名字和路径以及对应的url  存入对应的盘符中
 				Elements  imgels = doc.select("#main > div.row > article > div > a.thumbnail-container > img");
 			    for (Element el : imgels) {
@@ -54,11 +51,13 @@ public class CatchMM {
 					saveToFile(mm_ImgUrl, file_path,img_path);
 				}
 				//获取下一页
-				String newPath = els.get(0).attr("href");
-				System.out.println("已经爬取====>"+newPath+"完毕");
-				getUrl(newPath);
+			    System.out.println("已经爬取第"+count+"页");
+			    count++;
+				String newPath = "https://www.duodia.com/daxuexiaohua/list_"+count+".html";
+				getUrl(newPath,pageCount,count);
 
 			}else {
+				System.out.println("数据提取完毕");
 				return;
 			}
 		} catch (IOException e) {
@@ -67,8 +66,37 @@ public class CatchMM {
 		}
 	}
 	
-	
+	/**
+	 * 获取页面的总数
+	 * @param path  根据页面的规则，获取总共页数
+ 	 * @return
+	 * @throws IOException
+	 */
+	public static int getPageCount(String path) throws IOException {
+		  String url = "https://www.duodia.com/daxuexiaohua/";
+		  String  counturlsel = "#pagination-8510 > option:nth-child(11)";
+		  Document  doc = Jsoup.connect("https://www.duodia.com/daxuexiaohua/").get();
+		  String fpage =doc.select("#pagination-8510 > option:nth-child(11)").get(0).text();
+		  String str=fpage.trim();
+		  String str2="";
+		  if(str != null && !"".equals(str)){
+		  for(int i=0;i<str.length();i++){
+		  if(str.charAt(i)>=48 && str.charAt(i)<=57){
+		  str2+=str.charAt(i);
+		  }
+		  }
 
+		  }
+		return Integer.parseInt(str2);
+	}
+	
+	
+	/**
+	 * 存储文件
+	 * @param destUrl  原位置
+	 * @param filePath 区域分类
+	 * @param imgPath  目标位置
+	 */
 	public static void saveToFile(String destUrl,String filePath, String imgPath) {  
 		FileOutputStream fos = null;  
 		BufferedInputStream bis = null;  
